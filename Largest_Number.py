fdc56edbdb89fd9690fd4a5305a385905c660369
ArYTaN003 <112767473+ArYTaN003@users.noreply.github.com>
2023-08-31 20:55:50 +0530

    Update Largest_Number.py

--- Largest_Number.py
+++ Largest_Number.py
@@ -8,5 +8,5 @@ n3 = st.number_input("Number 3")
 larger = (n1 if (n1 > n2 and n1 > n3) else
      (n2 if (n2 > n1 and n2 > n3) else n3))
 st.subheader("Output")
-out  =  "The largest of 3 numbers is : "+larger
+out  =  "The largest of 3 numbers is : "+str(larger)
 st.write(out)
