9f93765cff18e5c8a6fbf04e62de2fe6c18f8a7c
ArYTaN003 <112767473+ArYTaN003@users.noreply.github.com>
2023-08-31 20:55:03 +0530

    Update Largest_Number.py

--- Largest_Number.py
+++ Largest_Number.py
@@ -1,14 +1,12 @@
 import streamlit as st
-st.write("""
-#Largest of Three Numbers
-This app Outputs the largest out of 3 numbers         
-""")
-st.header("Numbers")
+st.header("Largest of 3 Numbers")
+st.write("This app Outputs the largest out of 3 numbers")
+st.subheader("Numbers")
 n1 = st.number_input("Number 1")
 n2 = st.number_input("Number 2")
 n3 = st.number_input("Number 3")
 larger = (n1 if (n1 > n2 and n1 > n3) else
      (n2 if (n2 > n1 and n2 > n3) else n3))
-st.header("Output")
+st.subheader("Output")
 out  =  "The largest of 3 numbers is : "+larger
 st.write(out)
